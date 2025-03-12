36976312f21f3d85cd441abc7c5932bd65a82a03
Grayv3r3 <122377541+Grayv3r3@users.noreply.github.com>
2025-03-12 16:26:43 -0400

    Made it nearly impossible to win, lest you go against instructions.

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 import random
 import os
 
-x = random.randint(100,10000)
-print("Guess a number between 100 and 100000.")
+print("this game will lie to you.")
+x = random.randint(1000000,1000000000)
+print("Guess a number between 1 and 1.")
 guess == input("Guess: ")
 if guess == x:
   print("You win! Congrats")
