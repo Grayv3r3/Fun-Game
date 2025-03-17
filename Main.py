c0ec4cafcedb22313254ed3ad8c2c592d41c1900
Grayv3r3 <122377541+Grayv3r3@users.noreply.github.com>
2025-03-17 10:20:25 -0400

    Update Main.py
    silly

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import os
 
 print("this game will lie to you.")
 x = random.randint(1000000,1000000000)
-print("Guess a number between 1 and 1.")
+print("Guess a number between 1 and 1. But be careful, if you get the number wrong then something silly will happen.")
 guess == input("Guess: ")
 if guess == x:
   print("You win! Congrats")
